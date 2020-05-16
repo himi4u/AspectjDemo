028d4355e49cf5f8a352f17a04b5bb774499b623
RichardZhang <11123620058@qq.com>
2020-05-16 17:53:10 +0800

    组件化和页面初始代码

---
+++ module_lib_common/src/main/java/com/himi/module_lib_common/arouter/ARouterConstants.java
@@ -0,0 +1,17 @@
+package com.himi.module_lib_common.arouter;
+
+/**
+ * @ProjectName: AspectjDemo
+ * @Package: com.himi.module_lib_common.arouter
+ * @ClassName: ARouterConstants
+ * @Description:
+ * @Author: 奔跑的土掉渣
+ * @CreateDate: 2020/5/16 17:17
+ * @UpdateUser:
+ * @UpdateDate: 2020/5/16 17:17
+ * @UpdateRemark:
+ * @Version: 1.0
+ */
+public interface ARouterConstants {
+
+}
---
+++ module_main/src/main/java/com/himi/module_main/activity/MainActivity.java
@@ -0,0 +1,121 @@
+package com.himi.module_main.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.himi.module_main.R;
+import com.himi.module_main.adapter.ExamplePagerAdapter;
+import com.himi.module_main.databinding.ActivityMainBinding;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.CommonPagerTitleView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String[] CHANNELS = new String[]{"NOUGAT", "DONUT", "ECLAIR", "KITKAT"};
+    private List<String> mDataList = Arrays.asList(CHANNELS);
+    private ExamplePagerAdapter mExamplePagerAdapter = new ExamplePagerAdapter(mDataList);
+
+    private ViewPager mViewPager;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initView();
+
+    }
+
+    private void initView() {
+        mViewPager = binding.viewPager;
+        mViewPager.setAdapter(mExamplePagerAdapter);
+        initMagicIndicator1();
+
+    }
+
+    private void initMagicIndicator1() {
+        MagicIndicator magicIndicator = binding.magicIndicator1;
+        magicIndicator.setBackgroundColor(Color.BLACK);
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdjustMode(true);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+
+            @Override
+            public int getCount() {
+                return mDataList.size();
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                CommonPagerTitleView commonPagerTitleView = new CommonPagerTitleView(context);
+
+                // load custom layout
+                View customLayout = LayoutInflater.from(context).inflate(R.layout.simple_pager_title_layout, null);
+                final ImageView titleImg = (ImageView) customLayout.findViewById(R.id.title_img);
+                final TextView titleText = (TextView) customLayout.findViewById(R.id.title_text);
+                titleImg.setImageResource(R.mipmap.ic_launcher);
+                titleText.setText(mDataList.get(index));
+                commonPagerTitleView.setContentView(customLayout);
+
+                commonPagerTitleView.setOnPagerTitleChangeListener(new CommonPagerTitleView.OnPagerTitleChangeListener() {
+
+                    @Override
+                    public void onSelected(int index, int totalCount) {
+                        titleText.setTextColor(Color.WHITE);
+                    }
+
+                    @Override
+                    public void onDeselected(int index, int totalCount) {
+                        titleText.setTextColor(Color.LTGRAY);
+                    }
+
+                    @Override
+                    public void onLeave(int index, int totalCount, float leavePercent, boolean leftToRight) {
+                        titleImg.setScaleX(1.3f + (0.8f - 1.3f) * leavePercent);
+                        titleImg.setScaleY(1.3f + (0.8f - 1.3f) * leavePercent);
+                    }
+
+                    @Override
+                    public void onEnter(int index, int totalCount, float enterPercent, boolean leftToRight) {
+                        titleImg.setScaleX(0.8f + (1.3f - 0.8f) * enterPercent);
+                        titleImg.setScaleY(0.8f + (1.3f - 0.8f) * enterPercent);
+                    }
+                });
+
+                commonPagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mViewPager.setCurrentItem(index);
+                    }
+                });
+
+                return commonPagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                return null;
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+    }
+}
